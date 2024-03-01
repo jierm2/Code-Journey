45a7e0ccb3ac1897cda232dc83f518a30e37957d
Arvind Murty <10248018+arvind-murty@users.noreply.github.com>
2024-03-01 15:38:01 -0600

    added sandbox page

---
+++ frontend/src/scenes/sandbox/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Sandbox = () => {
+
+  return (
+    <div class="min-h-screen flex flex-auto items-center justify-between mx-auto p-4">
+    <iframe src="https://trinket.io/embed/python/d6665f17fa" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+</div>
+  );
+};
+
+export default Sandbox;
