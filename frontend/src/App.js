af2048b680cd259844948b3f4585e9800970e3ad
arnoldalinaggar <142763461+arnoldalinaggar@users.noreply.github.com>
2024-03-01 16:43:11 -0600

    Revert "Added a sandbox page"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Signup from './scenes/signup';
 import ForgotPassword from './scenes/forgotpassword';
 import Homepage from './scenes/homepage';
 import Navbar from './scenes/navbar';
-import Sandbox from './scenes/sandbox';
 function App() {
   return (
     <>
@@ -16,7 +15,6 @@ function App() {
 
           <Route path="/login" element={<SignIn/>} />
           <Route path="/signup" element={<Signup/>} />
-          <Route path="/sandbox" element={<Sandbox/>} />
           <Route path="/forgot-password" element={<ForgotPassword/>} />
 
       </Routes>
--- frontend/src/scenes/navbar/index.jsx
+++ frontend/src/scenes/navbar/index.jsx
@@ -25,9 +25,6 @@ function Navbar() {
             <li>
               <a href="/" className="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white">Home</a>
             </li>
-            <li>
-              <a href="/sandbox" className="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white">Sandbox</a>
-            </li>
 
             <li>
               <a href="/login" className="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white">Login</a>
