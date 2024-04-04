00b8094711e5ba9c762a5e13e15fbd6a038e75ea
Rajas Gandhi <rajasmgandhi@gmail.com>
2024-04-04 16:44:19 -0500

    updated styling for map page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,28 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import SignIn from './scenes/signin';
-import Signup from './scenes/signup';
-import ForgotPassword from './scenes/forgotpassword';
-import Homepage from './scenes/homepage';
-import Navbar from './scenes/navbar';
-import Sandbox from './scenes/sandbox';
-import MapPage from './scenes/map/map';
-import HelloWorld from './scenes/hello-world';
+import SignIn from "./scenes/signin";
+import Signup from "./scenes/signup";
+import ForgotPassword from "./scenes/forgotpassword";
+import Homepage from "./scenes/homepage";
+import Navbar from "./scenes/navbar";
+import Sandbox from "./scenes/sandbox";
+import MapPage from "./scenes/map/map";
+import HelloWorld from "./scenes/hello-world";
 
 function App() {
   return (
     <>
-            <Navbar/>
+      <Navbar />
 
       <Routes>
-      <Route path="/" element={<Homepage/>} />
+        <Route path="/" element={<Homepage />} />
 
-
-          <Route path="/map" element={<MapPage/>} />
-          <Route path="/login" element={<SignIn/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/sandbox" element={<Sandbox/>} />
-          <Route path="/forgot-password" element={<ForgotPassword/>} />
-          <Route path="/hello-world" element={<HelloWorld/>} />
-
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/sandbox" element={<Sandbox />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/hello-world" element={<HelloWorld />} />
       </Routes>
     </>
   );
