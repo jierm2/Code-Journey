17f7535319a3197e49579f8f11f8fb6473b06d83
Jimmy Miao <Jimmymiao08@gmail.com>
2024-05-29 21:49:03 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CodeJourney',
-        'USER': 'root',
-        'PASSWORD': 'Miaojier00',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -101,8 +101,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jimmymiao08@gmail.com'
-EMAIL_HOST_PASSWORD = 'smapnmqkpzrbaoje'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 # Password validation
